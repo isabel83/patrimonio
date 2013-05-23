5a879e0a507fa002acd278bb67dc0a999874cd81
Isabel Apezteguia <isaam9@hotmail.com>
2013-05-23 13:42:20 +0200

    cambio nombre del servicio y creando paquetes

---
+++ src/com/patrimonio/plantillas/client/LoginServiceAsync.java
@@ -0,0 +1,11 @@
+package com.patrimonio.plantillas.client;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+/**
+ * The async counterpart of <code>GreetingService</code>.
+ */
+public interface LoginServiceAsync {
+	void greetServer(String input, AsyncCallback<String> callback)
+			throws IllegalArgumentException;
+}
